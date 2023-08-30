db09b668dde70b20e8cf290c197f7db88c13009a
cartermusee <cartermusee@gmail.com>
2023-08-30 13:48:45 -0700

    is sibling

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_sibling - funcrion to find sibling
+ * @node: pointer to npde to find sibling
+ * Return: pointer to sibling
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	binary_tree_t *prnt = node->parent
+
+	if (node == NULL || node->parent == NULL)
+	{
+		return (NULL);
+	}
+	if (prnt->left == node && parent->right != NULL)
+	{
+		return (prnt->right);
+	}
+	if (prnt->right == node && prnt->left != NULL)
+	{
+		return (prnt->left);
+	}
+	return (NULL);
+}
