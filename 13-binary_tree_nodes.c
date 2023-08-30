d05c5063b2013d8a75e9ac98a456680654f1ff83
Mutisomary <marymutso2016@gmail.com>
2023-08-30 05:18:09 -0700

    Nodes with children

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_leaves - count the nodes with at least 1 child.
+ * binary_tree_nodes - count the nodes with at least 1 child.
  * @tree: pointer to the root node of the binary tree.
  * Return: 0 or number of nodes
  */
